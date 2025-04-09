fe6554bbe9a64b9357b636f0880f0de51f8a9edd
Bob Nguyen <bob@castalk.com>
2025-04-09 16:00:32 +0700

    feat: remove checkout mutation methods

--- saleor/graphql/checkout/schema.py
+++ saleor/graphql/checkout/schema.py
@@ -18,26 +18,6 @@ from ..core.fields import BaseField, ConnectionField, FilterConnectionField
 from ..core.scalars import UUID
 from ..payment.mutations import CheckoutPaymentCreate
 from .filters import CheckoutFilterInput
-from .mutations import (
-    CheckoutAddPromoCode,
-    CheckoutBillingAddressUpdate,
-    CheckoutComplete,
-    CheckoutCreate,
-    CheckoutCreateFromOrder,
-    CheckoutCustomerAttach,
-    CheckoutCustomerDetach,
-    CheckoutDeliveryMethodUpdate,
-    CheckoutEmailUpdate,
-    CheckoutLanguageCodeUpdate,
-    CheckoutLineDelete,
-    CheckoutLinesAdd,
-    CheckoutLinesDelete,
-    CheckoutLinesUpdate,
-    CheckoutRemovePromoCode,
-    CheckoutShippingAddressUpdate,
-    CheckoutShippingMethodUpdate,
-    OrderCreateFromCheckout,
-)
 from .resolvers import resolve_checkout, resolve_checkout_lines, resolve_checkouts
 from .sorters import CheckoutSortingInput
 from .types import (
@@ -115,31 +95,4 @@ class CheckoutQueries(graphene.ObjectType):
 
 
 class CheckoutMutations(graphene.ObjectType):
-    checkout_add_promo_code = CheckoutAddPromoCode.Field()
-    checkout_billing_address_update = CheckoutBillingAddressUpdate.Field()
-    checkout_complete = CheckoutComplete.Field()
-    checkout_create = CheckoutCreate.Field()
-    checkout_create_from_order = CheckoutCreateFromOrder.Field()
-    checkout_customer_attach = CheckoutCustomerAttach.Field()
-    checkout_customer_detach = CheckoutCustomerDetach.Field()
-    checkout_email_update = CheckoutEmailUpdate.Field()
-    checkout_line_delete = CheckoutLineDelete.Field(
-        deprecation_reason=(
-            f"{DEPRECATED_IN_3X_FIELD} Use `checkoutLinesDelete` instead."
-        )
-    )
-    checkout_lines_delete = CheckoutLinesDelete.Field()
-    checkout_lines_add = CheckoutLinesAdd.Field()
-    checkout_lines_update = CheckoutLinesUpdate.Field()
-    checkout_remove_promo_code = CheckoutRemovePromoCode.Field()
     checkout_payment_create = CheckoutPaymentCreate.Field()
-    checkout_shipping_address_update = CheckoutShippingAddressUpdate.Field()
-    checkout_shipping_method_update = CheckoutShippingMethodUpdate.Field(
-        deprecation_reason=(
-            f"{DEPRECATED_IN_3X_FIELD} Use `checkoutDeliveryMethodUpdate` instead."
-        )
-    )
-    checkout_delivery_method_update = CheckoutDeliveryMethodUpdate.Field()
-    checkout_language_code_update = CheckoutLanguageCodeUpdate.Field()
-
-    order_create_from_checkout = OrderCreateFromCheckout.Field()
