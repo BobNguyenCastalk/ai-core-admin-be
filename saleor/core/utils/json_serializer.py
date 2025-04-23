a6431f25be67ebb4393002061b106c3de3989106
Bob Nguyen <bob@castalk.com>
2025-04-23 15:52:58 +0700

    feat: remove redundant reference to money and weight

--- saleor/core/utils/json_serializer.py
+++ saleor/core/utils/json_serializer.py
@@ -1,10 +1,6 @@
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.serializers.json import Serializer as JsonSerializer
 from draftjs_sanitizer import SafeJSONEncoder
-from measurement.measures import Weight
-from prices import Money
-
-MONEY_TYPE = "Money"
 
 
 class Serializer(JsonSerializer):
@@ -15,11 +11,6 @@ class Serializer(JsonSerializer):
 
 class CustomJsonEncoder(DjangoJSONEncoder):
     def default(self, obj):
-        if isinstance(obj, Money):
-            return {"_type": MONEY_TYPE, "amount": obj.amount, "currency": obj.currency}
-        # Mirror implementation of django_measurement.MeasurementField.value_to_string
-        if isinstance(obj, Weight):
-            return f"{obj.value}:{obj.unit}"
         return super().default(obj)
 
 
